89917730b17bd218bec6dd8d77198cfd8ec277f5
dboehm <drdboehm@gmx.de>
2025-04-24 09:47:35 +0200

    Delete MyEntity GreetingResource.java and disable GreetingResourceTest

--- src/test/java/de/jamsintown/GreetingResourceTest.java
+++ src/test/java/de/jamsintown/GreetingResourceTest.java
@@ -1,20 +1,23 @@
 package de.jamsintown;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@QuarkusTest
+// @QuarkusTest
 class GreetingResourceTest {
-    @Test
+    @ConfigProperty(name = "publisher.name")
+    String publisherName;
+   // @Test
     void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is("Hello from Quarkus " + publisherName + "!"));
     }
 
 }
