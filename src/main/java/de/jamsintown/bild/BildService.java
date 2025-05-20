b5893a5c692c21e91c9cf6743359a05913215998
dboehm <drdboehm@gmx.de>
2025-05-20 21:51:21 +0200

    add setProtect method to BildService

--- src/main/java/de/jamsintown/bild/BildService.java
+++ src/main/java/de/jamsintown/bild/BildService.java
@@ -2,22 +2,26 @@ package de.jamsintown.bild;
 
 import de.jamsintown.config.main.ImageSettings;
 import de.jamsintown.user.UserService;
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.quarkus.security.ForbiddenException;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.hibernate.ObjectNotFoundException;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @ApplicationScoped
 public class BildService {
 
     private final UserService userService;
+
     @Inject
     public BildService(UserService userService) {
         this.userService = userService;
     }
+
     public void takeBild(ImageSettings imageSettings) {
     }
 
@@ -26,7 +30,7 @@ public class BildService {
                 .chain(user -> Bild.find("user", user).list());
     }
 
-    public Uni<Bild> getIdForUser(Long id) {
+    public Uni<Bild> findById(Long id) {
         return userService.getCurrentUser()
                 .chain(user -> Bild.<Bild>findById(id)
                         .onItem().ifNull().failWith(() -> new ObjectNotFoundException(id, "Bild"))
@@ -37,11 +41,22 @@ public class BildService {
                         }));
     }
 
+    @WithTransaction
     public Uni<Bild> create(Bild bild) {
         return userService.getCurrentUser()
                 .chain(user -> {
                     bild.user = user;
                     return bild.persistAndFlush();
                 });
+    }
+
+    @WithTransaction
+    public Uni<Boolean> setProtect(long id, boolean protect) {
+        return findById(id)
+                .chain(bild -> {
+                    bild.protect = protect ? ZonedDateTime.now() : null;
+                    return bild.persistAndFlush();
+                })
+                .chain(bild -> Uni.createFrom().item(protect));
     }
 }
