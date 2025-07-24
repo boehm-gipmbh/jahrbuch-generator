40382078440f98347008bf080bf2f92208dbaafe
dboehm <drdboehm@gmx.de>
2025-07-24 08:15:32 +0200

    quinoa dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("io.quarkus:quarkus-smallrye-jwt")
     implementation("io.quarkus:quarkus-smallrye-jwt-build")
     implementation("org.gphoto:gphoto2-java:1.5-SNAPSHOT")
+    implementation("io.quarkiverse.quinoa:quarkus-quinoa:2.5.5")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
@@ -38,14 +39,37 @@ group = "de.jamsintown"
 version = "1.0.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
 }
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.compilerArgs.add("-parameters")
 }
+
+//tasks.withType<io.quarkus.gradle.tasks.QuarkusBuild> {
+//    systemProperty("quarkus.native.builder-image.java-home-env-var", "JAVA_HOME")
+//    jvmArgs("-Xmx8g", "-XX:MaxMetaspaceSize=2g")
+//}
+
+
+//// build.gradle.kts
+//tasks.register<Exec>("npmBuild") {
+//    workingDir = file("src/main/frontend")
+//    commandLine("${System.getProperty("user.home")}/.nvm/versions/node/v20.17.0/bin/npm", "run", "build")
+//}
+//
+//tasks.register<Copy>("copyFrontend") {
+//    dependsOn("npmBuild")
+//    from("src/main/frontend/build")
+//    into("build/classes/frontend")
+//}
+//
+//tasks.processResources {
+//    dependsOn("copyFrontend")
+//}
