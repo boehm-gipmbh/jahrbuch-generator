79d24b6baf6c6e4c73de62595518766a22b82db4
DrDBoehm <drdboehm@jamsintown.de>
2026-05-16 20:17:41 +0200

    fix: @RegisterForReflection für AnnouncementResult (native image Jackson)

--- src/main/java/de/jamsintown/announcement/AnnouncementResult.java
+++ src/main/java/de/jamsintown/announcement/AnnouncementResult.java
@@ -1,12 +1,16 @@
 package de.jamsintown.announcement;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.List;
 
+@RegisterForReflection
 public class AnnouncementResult {
 
     public int sent;
     public int failed;
     public List<String> errors;
+
+    public AnnouncementResult() {}
 
     public AnnouncementResult(int sent, int failed, List<String> errors) {
         this.sent = sent;
