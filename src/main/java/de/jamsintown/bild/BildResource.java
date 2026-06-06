f5f9b70363b59d54dff37a2d62f62cdad8d2a0c8
DrDBoehm <drdboehm@jamsintown.de>
2026-06-06 13:34:04 +0200

    fix: clearCaption für admin ohne Ownership-Check – findById prüft Gruppe/User

--- src/main/java/de/jamsintown/bild/BildResource.java
+++ src/main/java/de/jamsintown/bild/BildResource.java
@@ -100,14 +100,14 @@ public class BildResource {
 
     @DELETE
     @Path("/{id}/caption")
-    @RolesAllowed("group-admin")
+    @RolesAllowed("admin")
     public Uni<Void> clearCaption(@PathParam("id") long id) {
-        return bildService.clearCaption(id);
+        return bildService.clearCaptionAdmin(id);
     }
 
     @DELETE
     @Path("/captions")
-    @RolesAllowed("group-admin")
+    @RolesAllowed("admin")
     public Uni<Integer> clearAllCaptions() {
         return bildService.clearAllCaptions();
     }
